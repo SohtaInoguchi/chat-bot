93247add72b58d91568e7b70f1dc90788370c1a5
JohnnyDamon <53366209+JohnnyDamon@users.noreply.github.com>
2022-10-22 22:52:04 +0900

    add function to get input message

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-
+const nlpStarter = require("./index");
+console.log("nlpStart???", nlpStarter);
 app.listen(port, () => {
+  nlpStarter();
   console.log(`App listening port ${port}`);
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState('');
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setMessage(e.target.value);
+    console.log(e.target.value);
+  }
+
+  const sendMessage = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
+    e.preventDefault();
+    console.log("send message clicked");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Let's create chatbot
-        </a>
-      </header>
+    <div>
+      <label htmlFor='message'>Input message</label>
+      <input type='text' id='message' name='message' onChange={(e) => handleOnChange(e)}></input>
+      <button id='send-button' onClick={sendMessage}>Send</button>
     </div>
   );
 }
